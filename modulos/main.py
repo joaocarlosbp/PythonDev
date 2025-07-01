bc4a97bfbc1ef365272660f35b5ccc62966ed9d2
joaocarlosbp <joaocarlosbp22@gmail.com>
2025-07-01 03:02:29 -0300

    fim de aula

---
+++ modulos/main.py
@@ -0,0 +1,15 @@
+#progama principal
+import math_operations
+from math_operations import multiply, divide
+import string_utils
+# Importando o módulo math_operations
+print(math_operations.sum(5, 5))
+print(math_operations.subtract(10, 5))
+# Importando funções específicas do módulo
+# math_operations
+print(multiply(3, 7))
+print(divide(10, 2))
+
+print(string_utils.capitalize("hello"))
+print(string_utils.count("hello world"))
+print(string_utils.reverse_string("hello world"))
---
+++ modulos/math_operations.py
@@ -0,0 +1,15 @@
+#modulo de operações matemáticas
+def sum (x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    if y != 0:
+        return x / y
+    else:   
+        raise ValueError("Cannot divide by zero")
